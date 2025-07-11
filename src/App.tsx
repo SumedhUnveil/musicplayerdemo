f4b9d796278fceb7d0b5c02326f50d82dae22fba
SumedhUnveil <sumedh.bajracharya@unveil.health>
2025-07-11 17:44:20 +0545

    update: add title

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Player from './components/Player';
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 to-blue-900 flex items-center justify-center">
+      Media Player
       <Player />
     </div>
   );
